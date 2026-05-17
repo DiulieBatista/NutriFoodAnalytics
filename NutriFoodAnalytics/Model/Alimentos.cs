a4215f4146d4d891c31ee398295634d995dc7c74
Diulie <diuliemileide7@gmail.com>
2026-05-17 19:05:58 -0300

    Implemtação da pasta model e criação da calsse alimentos

---
+++ NutriFoodAnalytics/Model/Alimentos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NutriFoodAnalytics.Models
+{
+    public class Alimento
+    {
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = string.Empty;
+
+
+        [JsonPropertyName("name")]
+        public string Nome { get; set; } = string.Empty;
+
+
+        [JsonPropertyName("calories")]
+        public double Calorias { get; set; }
+
+
+        [JsonPropertyName("serving_size_g")]
+        public double PorcaoGramas { get; set; }
+
+        [JsonPropertyName("fat_total_g")]
+        public double GorduraTotal { get; set; }
+
+        [JsonPropertyName("fat_saturated_g")]
+        public double GorduraSaturada { get; set; }
+
+        [JsonPropertyName("protein_g")]
+        public double Proteina { get; set; }
+
+
+        [JsonPropertyName("sodium_mg")]
+        public double Sodio { get; set; }
+
+
+        [JsonPropertyName("potassium_mg")]
+        public double Potassio { get; set; }
+
+
+        [JsonPropertyName("cholesterol_mg")]
+        public double Colesterol { get; set; }
+
+
+        [JsonPropertyName("carbohydrates_total_g")]
+        public double Carboidratos { get; set; }
+
+
+        [JsonPropertyName("fiber_g")]
+        public double Fibras { get; set; }
+
+
+        [JsonPropertyName("sugar_g")]
+        public double Acucar { get; set; }
+
+
+        public DateTime DataValidacao { get; set; } = DateTime.UtcNow;
+    }
+
+
+}
