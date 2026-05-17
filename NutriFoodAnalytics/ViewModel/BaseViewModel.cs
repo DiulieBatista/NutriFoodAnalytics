0986702d2a19a4e3dffb117d6e5be2e9eacd7389
Diulie <diuliemileide7@gmail.com>
2026-05-17 19:12:08 -0300

    Implementação da classe BaseViewmodel dentro da basta View Model

---
+++ NutriFoodAnalytics/ViewModel/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriFoodAnalytics.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(
+            [CallerMemberName] string? nome = null)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(nome));
+        }
+    }
+}
+
+
