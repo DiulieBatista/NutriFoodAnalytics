39c909fca312359ca8b6163b077bce0cb1068abb
Diulie <diuliemileide7@gmail.com>
2026-05-17 18:54:05 -0300

    implementação da classe Rlay command dentro da pasta Command  e modificação da pasta main Window.xaml.cs para dentro da pasta Views .

--- NutriFoodAnalytics/Command/RelayCommand.cs
+++ NutriFoodAnalytics/Command/RelayCommand.cs
@@ -3,10 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NutriFoodAnalytics.Command
 {
-    class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Func<Task> _executeAsync;
+        private readonly Func<bool>? _canExecute;
+        private readonly Action? _execute;
+
+
+        public RelayCommand(Func<Task> executeAsync, Func<bool>? canExecute = null)
+        {
+            _executeAsync = executeAsync;
+            _canExecute = canExecute;
+        }
+
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public async void Execute(object? parameter)
+        {
+            if (_executeAsync != null)
+                await _executeAsync();
+            else
+                _execute?.Invoke();
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
     }
 }
